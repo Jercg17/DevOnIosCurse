741a84faf822cfc035e66d86d892895ae221f08d
Jesus Ramirez <62226844+Jercg17@users.noreply.github.com>
2021-06-19 21:11:06 -0500

    Actividad 3

---
+++ Actividad3.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+var dato1 :Int = 12
+dato1 = 12 
+var dato2 :Float = 1.2
+dato2 = 1.2
+var dato3 :String = String (" Hola ") 
+var dato4 = (2,2) 
+var dato4 = (" Hola", "Hola 2")
+
+Var arre; Array <Int> = Array <Int> ()
+arre.append(1)
+arre.append(2)
+arre.append(3)
+arre.append(4)
+arre.append(5)
+arre.append(6)
+arre.append(7)
+arre.append(8)
+arre.append(9)
+arre.append(10)
+
+var diccionario; Dictionary<Int, String = Dictionary<Int, String> ()
+diccionario = [1: "Lunes"]
+diccionario = [2: "Martes"]
+diccionario = [3: "Miercoles"]
+diccionario = [4: "Jueves"]
+diccionario = [5: "Viernes"]
+diccionario = [6: "Sabado"]
+diccionario = [7: "Domingo"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
