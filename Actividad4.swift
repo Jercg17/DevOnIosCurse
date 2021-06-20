693f7a6badd5d8bbb1d1612832374a7283d26acf
Jesus Ramirez <62226844+Jercg17@users.noreply.github.com>
2021-06-19 21:25:25 -0500

    Actividad 4

---
+++ Actividad4.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+
+var datos = [3,6,9,2,4,1]
+
+for dato in datos {
+    print (dato) 
+}
+
+for dato in datos {
+    if dato < 5 {
+        print (dato)
+    }
+}
+
+func Suma(val1:Int, val2:Int)-> Int {
+    return val1 + val2
+}
+
+
+
+
+
+
