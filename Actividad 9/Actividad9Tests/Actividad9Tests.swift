f8cc8e2510246545717201627088a74827c6ff99
Jesus Ramirez <62226844+Jercg17@users.noreply.github.com>
2021-07-05 19:28:46 -0500

    Actividad 9: Prototipo y pantalla de navegación

---
+++ Actividad 9/Actividad9Tests/Actividad9Tests.swift
@@ -0,0 +1,27 @@
+
+
+import XCTest
+@testable import Actividad9
+
+class Actividad9Tests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+
+    func testExample() {
+        
+    }
+
+    func testPerformanceExample() {
+        
+        self.measure {
+            
+        }
+    }
+
+}
---
+++ Actividad 9/Actividad9UITests/Actividad9UITests.swift
@@ -0,0 +1,25 @@
+
+
+import XCTest
+
+class Actividad9UITests: XCTestCase {
+
+    override func setUp() {
+      
+        continueAfterFailure = false
+
+      
+        XCUIApplication().launch()
+
+       
+    }
+
+    override func tearDown() {
+       
+    }
+
+    func testExample() {
+       
+    }
+
+}
