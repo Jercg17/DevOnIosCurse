02fc2350869e662f9173fba23e309fdf291a4a73
Jesus Ramirez <62226844+Jercg17@users.noreply.github.com>
2021-07-15 21:22:24 -0500

    Actividad 13

---
+++ Actividad 13/VideoNoticia/VideoNoticia/NoticiasVC.swift
@@ -0,0 +1,28 @@
+
+import UIKit
+
+class NoticiasVC: UITableViewController {
+
+    var noticias:[String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let inset = UIEdgeInsets(top: 60, left: 0, bottom: 0, right: 0);
+        self.tableView.contentInset = inset
+        self.tableView.scrollIndicatorInsets = inset
+        LeerNoticias().getNoticias { (datos) in
+              self.noticias = datos
+              self.tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.noticias.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = (tableView.dequeueReusableCell(withIdentifier: "celda") as UITableViewCell?)!
+        cell.textLabel?.text = self.noticias[indexPath.row]
+        return cell
+    }
+}
